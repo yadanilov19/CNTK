3cde62e066ceb57cf66e19ea0d26add65f0198e9
Ivan Rodriguez <t-ivrodr@microsoft.com>
2017-05-09 11:13:38 +0200

    Addressing comments in CR

--- Tests/EndToEndTests/CNTKv2Python/Examples/ConvNet_MNIST_test.py
+++ Tests/EndToEndTests/CNTKv2Python/Examples/ConvNet_MNIST_test.py
@@ -7,6 +7,7 @@
 import os, sys
 import numpy as np
 from cntk.device import try_set_default_device, gpu
+from cntk.ops.tests.ops_test_utils import cntk_device
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(abs_path, "..", "..", "..", "..", "Examples", "Image", "Classification", "ConvNet", "Python" ))
@@ -16,7 +17,8 @@ from ConvNet_MNIST import convnet_mnist
 TOLERANCE_ABSOLUTE = 1E-2
 
 def test_convnet_mnist_error(device_id):
-    from cntk.ops.tests.ops_test_utils import cntk_device
+    if cntk_device(device_id).type() != DeviceKind_GPU:
+        pytest.skip('test only runs on GPU')
     try_set_default_device(cntk_device(device_id))
 
     error = convnet_mnist(epoch_size=5000, minibatch_size=32, max_epochs=10)
